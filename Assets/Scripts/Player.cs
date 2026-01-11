86cb43d37edb05967bb18f0c2c166241400d6749
trannhatdang <hihihibly@gmail.com>
2026-01-11 19:32:00 +0700

    moving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,25 +7,51 @@ public class Player : MonoBehaviour
 	[SerializeField] float _hortSpeed = 2f;
 	[SerializeField] float _vertSpeed = 1f;
 
-	float _hort;
-	float _vert;
+	Vector2 _velocity;
+	bool _canAttack = true;
 
 	Rigidbody2D _rb;
+	Animator _anim;
 
 	void Start()
 	{
 		_rb = GetComponent<Rigidbody2D>();
+		_anim = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		_rb.MovePosition(_rb.position + new Vector2(_hort * _hortSpeed, _vert * _vertSpeed) * Time.fixedDeltaTime);
+		if(_velocity.sqrMagnitude > 0)
+		{
+			_anim.SetBool("Moving", true);
+		}
+		else
+		{
+			_anim.SetBool("Moving", false);
+		}
+
+		_rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime);
 	}
 
 	public void Move(InputAction.CallbackContext context)
 	{
-		_hort = context.ReadValue<Vector2>().x;
-		_vert = context.ReadValue<Vector2>().y;
+		float hort = context.ReadValue<Vector2>().x * _hortSpeed;
+		float vert = context.ReadValue<Vector2>().y * _vertSpeed;
+
+		_velocity = new Vector2(hort, vert);
+	}
+
+	public void Attack(InputAction.CallbackContext context)
+	{
+		if(!_canAttack) return;
+
+		_anim.SetTrigger("Attack");
+		_canAttack = false;
+	}
+
+	public void SetAttack()
+	{
+		_canAttack = true;
 	}
 }
